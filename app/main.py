0c68809bc18e905fa1667e817c1fa8178868a6fe
ginaapaola <ginapaolamc07@gmail.com>
2026-05-29 15:38:17 -0500

    cors url front

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://forecash-chi.vercel.app/login",
         "https://forecash-ginaapaola-gina-paola-s-projects.vercel.app",
         "https://forecash-dyq30u5bo-gina-paola-s-projects.vercel.app"
     ],
